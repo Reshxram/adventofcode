f364445df8277d918a86bc242df5bc9de18729ea
Reshxram <74835069+Reshxram@users.noreply.github.com>
2022-12-15 11:30:26 +0100

    Solved 10.1

--- 10/10.py
+++ 10/10.py
@@ -1,23 +1,28 @@
-with open("test.txt") as f:
+with open("input.txt") as f:
     ls = [x.strip() for x in f.readlines()]
 
 
-def increase_tick(signal, tick):
-    tick += 1
-    if (tick + 20) % 40 == 0:
-        print("Tick:", tick, "Signal:", signal, "Tick * Signal:", tick * signal)
-    return tick
-
 def signal_strengths():
     tick = 0
-    signal = 0
+    signal = 1
+    signals = 0
     for l in ls:
         if l[:4] == "noop":
-            tick = increase_tick(signal, tick)
+            tick += 1
+            if (tick + 20) % 40 == 0:
+                signals += tick * signal
+                print("Tick:", tick, "Signal:", signal, "Tick * Signal:", tick * signal)
         elif l[:4] == "addx":
-            tick = increase_tick(signal, tick)
-            tick = increase_tick(signal, tick)
+            tick += 1
+            if (tick + 20) % 40 == 0:
+                signals += tick * signal
+                print("Tick:", tick, "Signal:", signal, "Tick * Signal:", tick * signal)
+            tick += 1
+            if (tick + 20) % 40 == 0:
+                signals += tick * signal
+                print("Tick:", tick, "Signal:", signal, "Tick * Signal:", tick * signal)
             signal += int(l[5:])
+    return signals
 
 
-signal_strengths()
+print(signal_strengths())
